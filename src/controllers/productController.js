279c685b48ddb93d8cd0ea86e47d1f53075ed5b3
LucasSanjurjo <sanjurjolucasdamian@live.com>
2022-09-20 15:25:16 -0300

    terminando clase 22

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,73 +1,80 @@
 const fs = require("fs");
 const path = require("path");
 
-function findAll(){
-    const jsonData = fs.readFileSync(path.join(__dirname, "../data/products.json"));
-    const data = JSON.parse(jsonData);
-    return data
+function findAll() {
+  const jsonData = fs.readFileSync(
+    path.join(__dirname, "../data/products.json")
+  );
+  const data = JSON.parse(jsonData);
+  return data;
 }
 
-function writeFile(data){
-   const dataString = JSON.stringify(data, null, 4);
-   fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString);
+function writeFile(data) {
+  const dataString = JSON.stringify(data, null, 4);
+  fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString);
 }
 
 const controller = {
-    list: (req, res)=>{
-        const data = findAll()
-        res.render("menu-products", { products: data })
-    },
-    detail: (req, res)=>{
-        const data = findAll();
-        const platoEncontrado = data.find(function(plato){
-            return plato.id == req.params.id
-        })
+  list: (req, res) => {
+    const data = findAll();
+    res.render("menu-products", { products: data });
+  },
 
-        res.render("product-detail", { plato: platoEncontrado })
-    },
-    create: (req, res)=>{
-        res.render("product-create-form");
-    },
-    store: (req, res)=> {
-        const data = findAll()
+  detail: (req, res) => {
+    const data = findAll();
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
 
-        const newProduct = {
-            id: data.length + 3,
-            name: req.body.name,
-            price: Number(req.body.price),
-            description: req.body.description
-        }
+    res.render("product-detail", { plato: platoEncontrado });
+  },
 
-        data.push(newProduct);
+  create: (req, res) => {
+    res.render("product-create-form");
+  },
 
-        writeFile(data)
+  store: (req, res) => {
+    const data = findAll();
 
-        res.redirect("/products/create");
-    },
-    edit: (req, res)=>{
-        const data = findAll()
+    const newProduct = {
+      id: data.length ++,
+      name: req.body.name,
+      price: Number(req.body.price),
+      description: req.body.description,
+    };
 
-        const platoEncontrado = data.find(function(plato){
-            return plato.id == req.params.id
-        })
+    data.push(newProduct);
 
-        res.render("product-update-form", { plato: platoEncontrado });
-    },
-    update: (req, res)=>{
-        const data = findAll()
+    writeFile(data);
 
-        const platoEncontrado = data.find(function(plato){
-            return plato.id == req.params.id
-        })
+    res.redirect("/products/create");
+  },
 
-        platoEncontrado.name = req.body.name;
-        platoEncontrado.price = req.body.price;
-        platoEncontrado.description = req.body.description;
+  edit: (req, res) => {
+    const data = findAll();
 
-        writeFile(data);
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
 
-        res.redirect("/products/list");
-    }
-}
+    res.render("product-update-form", { plato: platoEncontrado });
+  },
+
+  update: (req, res) => {
+    const data = findAll();
+
+    const platoEncontrado = data.find(function (plato) {
+      return plato.id == req.params.id;
+    });
+
+    platoEncontrado.name = req.body.name;
+    platoEncontrado.price = req.body.price;
+    platoEncontrado.description = req.body.description;
+
+    writeFile(data);
+
+    res.redirect("/products/list");
+  },
+};
 
 module.exports = controller;
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router()
 const productController = require("../controllers/productController");
 
-router.get("/list", productController.list);
-router.get("/detail/:id", productController.detail);
+router.get("/list", productController.list); // solicito la vista
+router.get("/detail/:id", productController.detail); // solicito la vista de un producto identificado
 router.get("/create", productController.create);
 router.post("/create", productController.store);
 router.get("/edit/:id", productController.edit);
